7df0bbc8880da324dffa69aaea871fc31b987e21
LeeJiWon <dlwldnjs1009@gmail.com>
2023-01-24 23:50:08 +0900

    prometheus grafana modification

--- backend/income/models.py
+++ backend/income/models.py
@@ -2,7 +2,6 @@ import uuid
 from user.models import User
 from django.db import models
 
-
 class Income(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False)  # PK
     user = models.ForeignKey(User, on_delete=models.CASCADE)
